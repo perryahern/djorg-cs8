ff73f6ce3d1e91256ad734b5dd7e027f90f74b83
perryahern <pahern@gmail.com>
2018-06-25 17:46:14 -0400

    Bookmark app and model added, data saved

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,11 +3,10 @@ from uuid import uuid4
 
 # Create your models here.
 
-class Note(models.Model):
-  #TODO: Add user/author who created it
-  #TODO: Tagging system or categories
+class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=200)
-  content = models.TextField(blank=True)
+  url = models.URLField(max_length=200)
+  note = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
--- djorg/settings.py
+++ djorg/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmarks',
+    'notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
