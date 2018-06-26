8ecf7f937a83a2abe7b944f874d768d5d9017a4e
perryahern <pahern@gmail.com>
2018-06-26 17:40:30 -0400

    Correction to Bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -8,7 +8,7 @@ class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=200)
   url = models.URLField(max_length=200)
-  note = models.TextField()
+  note = models.TextField(blank=True)
   category = models.CharField(max_length=20)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
