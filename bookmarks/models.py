83d76d17959365a6551acb1de437535143a7b4e6
perryahern <pahern@gmail.com>
2018-06-26 14:48:14 -0400

    Lecture updates to Note and my updates to Bookmark; admin working for both

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,5 +9,9 @@ class Bookmark(models.Model):
   title = models.CharField(max_length=200)
   url = models.URLField(max_length=200)
   note = models.TextField()
+  category = models.CharField(max_length=20)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
+
+class PrivateBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Note(models.Model):
-  #TODO: Add user/author who created it
-  #TODO: Tagging system or categories
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=200)
   content = models.TextField(blank=True)
+  category = models.CharField(max_length=20)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
